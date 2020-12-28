430d1eb36b5e36c01606c9d982fb09758ca5e819
aamiryaqoob <aamiryaqoob965@gmail.com>
2020-12-28 18:06:03 +0500

    program3

---
+++ script.js
@@ -0,0 +1,5 @@
+//Write a js program that gets person height from user in inches and convert it into centimeters using the formula heightINCentimeters = 2.54 * height,console the output.
+
+var heightInInches = prompt("Enter your height in inches");
+var heightInCentimeters = 2.54 * heightInInches;
+console.log("Your height is " + heightInCentimeters +  " Cm");
