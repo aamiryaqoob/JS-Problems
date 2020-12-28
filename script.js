12ee4445ba2066a3c82b768156053293020d0f52
aamiryaqoob <aamiryaqoob965@gmail.com>
2020-12-28 18:12:44 +0500

    program4

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+var input = prompt("Enter a number(integer)");
+if (input%2==0) {
+    alert("Your number is even!")
+} else if(input%2!=0){
+    alert("Your number is odd!")
+}
+else{
+    alert("Invalid");
+}
