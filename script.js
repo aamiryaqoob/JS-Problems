8e8383bee4f591726dd5112e3148c280c78aeba8
aamiryaqoob <aamiryaqoob965@gmail.com>
2020-12-28 18:09:43 +0500

    program3

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+var sub1 = prompt("Enter your 1st subject marks");
+var sub2 = prompt("Enter your 2nd subject marks");
+var sub3 = prompt("Enter your 3rd subject marks");
+var averageMarks = (sub1+sub2+sub3)/3;
+if (averageMarks>=80) {
+    alert("You are above standerd Admission granted!");
+} else {
+    alert("Bed luck!");}
